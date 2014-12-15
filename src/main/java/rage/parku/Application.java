c7c02b46558ade00734b3fbb355d6532ce404589
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-12-15 12:29:34 +0200

    Added hsql database to pom.xml

--- src/main/java/rage/parku/Application.java
+++ src/main/java/rage/parku/Application.java
@@ -1,4 +1,4 @@
-package rage.parkour;
+package rage.parku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
--- src/test/java/rage/parku/ApplicationTests.java
+++ src/test/java/rage/parku/ApplicationTests.java
@@ -1,5 +1,6 @@
-package rage.parkour;
+package rage.parku;
 
+import rage.parku.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;

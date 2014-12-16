7a411987ca96e8c2256c9772d90029a952399fa5
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-12-16 14:14:46 +0200

    generalizing QuestionCreator

--- src/main/java/rage/parqu/questioncreator/BankQuestionCreator.java
+++ src/main/java/rage/parqu/questioncreator/BankQuestionCreator.java
@@ -9,33 +9,44 @@ import rage.parqu.domain.Question;
 import rage.parqu.util.Randomizer;
 
 public class BankQuestionCreator implements QuestionCreator{
+    
+    private int cash;
+    private int value;
+    private int border;
 
     @Override
     public Question createQuestion() {
-        MustacheFactory mf = new DefaultMustacheFactory();
+        MustacheFactory mf = new DefaultMustacheFactory();         
         Mustache mustache = mf.compile("bank.mustache");
         
+        HashMap<String, Object> scopes = setUpScope();
+        
+        StringWriter writer = new StringWriter();
+        mustache.execute(writer, scopes);
+        
+        Question question = setUpQuestion(writer);
+        question.setCode(writer.getBuffer().toString());
+        
+        return question;
+    }
+
+    private Question setUpQuestion(StringWriter writer) {
+        Question question = new Question();
+        question.setQuestionText("What is the printed number?");
+        question.setAnswers("" + (cash + border), "" + (cash + value), "" + (value + border), "" + cash);
+        question.setCorrectAnswer(determineRightAnswer(cash,value,border));
+        return question;
+    }
+
+    private HashMap<String, Object> setUpScope() {
         HashMap<String, Object> scopes = new HashMap<>();
-        
-        int cash = Randomizer.randomIntegerParameter();
-        int value = Randomizer.randomIntegerParameter();
-        int border = Randomizer.randomIntegerParameter();
-        
+        cash = Randomizer.randomIntegerParameter();
+        value = Randomizer.randomIntegerParameter();
+        border = Randomizer.randomIntegerParameter();
         scopes.put("cash", cash);
         scopes.put("value", value);
         scopes.put("border", border);
-        
-        StringWriter writer = new StringWriter();
-        
-        mustache.execute(writer, scopes);
-        
-        Question question = new Question();
-        question.setCode(writer.getBuffer().toString());
-        question.setCorrectAnswer(determineRightAnswer(cash,value,border));
-        question.setQuestionText("What is the printed number?");
-        question.setAnswers("" + (cash + border), "" + (cash + value), "" + (value + border), "" + cash);
-        
-        return question;
+        return scopes;
     }
 
     private String determineRightAnswer(int cash,int value, int border) {

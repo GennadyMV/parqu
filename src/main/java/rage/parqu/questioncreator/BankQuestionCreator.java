18aab606f11db47c868f5e99caf47af08df159d2
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-12-16 15:18:31 +0200

    General QuestionCreator and modifying BankQuestion to extend it

--- src/main/java/rage/parqu/questioncreator/BankQuestionCreator.java
+++ src/main/java/rage/parqu/questioncreator/BankQuestionCreator.java
@@ -1,60 +1,49 @@
 package rage.parqu.questioncreator;
 
-import com.github.mustachejava.DefaultMustacheFactory;
-import com.github.mustachejava.Mustache;
-import com.github.mustachejava.MustacheFactory;
-import java.io.StringWriter;
 import java.util.HashMap;
 import rage.parqu.domain.Question;
 import rage.parqu.util.Randomizer;
 
-public class BankQuestionCreator implements QuestionCreator{
+public class BankQuestionCreator extends QuestionCreator{
     
     private int cash;
     private int value;
     private int border;
 
+    public BankQuestionCreator() {
+        super.setTemplateName("bank.mustache");
+    }
+
     @Override
-    public Question createQuestion() {
-        MustacheFactory mf = new DefaultMustacheFactory();         
-        Mustache mustache = mf.compile("bank.mustache");
-        
-        HashMap<String, Object> scopes = setUpScope();
-        
-        StringWriter writer = new StringWriter();
-        mustache.execute(writer, scopes);
-        
-        Question question = setUpQuestion(writer);
-        question.setCode(writer.getBuffer().toString());
-        
-        return question;
-    }
-
-    private Question setUpQuestion(StringWriter writer) {
-        Question question = new Question();
-        question.setQuestionText("What is the printed number?");
-        question.setAnswers("" + (cash + border), "" + (cash + value), "" + (value + border), "" + cash);
-        question.setCorrectAnswer(determineRightAnswer(cash,value,border));
-        return question;
-    }
-
-    private HashMap<String, Object> setUpScope() {
-        HashMap<String, Object> scopes = new HashMap<>();
+    protected void randomizeParameters() {
         cash = Randomizer.randomIntegerParameter();
         value = Randomizer.randomIntegerParameter();
         border = Randomizer.randomIntegerParameter();
+    }  
+    
+    @Override
+    protected Question setUpQuestionAndAnswers() {
+        Question question = new Question();
+        question.setQuestionText("What is the printed number?");
+        question.setAnswers("" + (cash + border), "" + (cash + value), "" + (value + border), "" + cash);
+        return question;
+    }
+
+    @Override
+    protected HashMap<String, Object> setUpScope() {
+        HashMap<String, Object> scopes = new HashMap<>();
         scopes.put("cash", cash);
         scopes.put("value", value);
         scopes.put("border", border);
         return scopes;
     }
 
-    private String determineRightAnswer(int cash,int value, int border) {
+    @Override
+    protected String determineRightAnswer() {
         if(value > border){
             return "" + (cash + border);
         } else {
             return "" + (cash + value);
         }
     }
-    
 }
--- src/main/java/rage/parqu/questioncreator/QuestionCreator.java
+++ src/main/java/rage/parqu/questioncreator/QuestionCreator.java
@@ -1,9 +1,44 @@
 package rage.parqu.questioncreator;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+import java.io.StringWriter;
+import java.util.HashMap;
 import rage.parqu.domain.Question;
 
-public interface QuestionCreator {
+public abstract class QuestionCreator {
     
-    public Question createQuestion();
+    private String templateName;
     
+    public Question createQuestion() {
+        MustacheFactory mf = new DefaultMustacheFactory();         
+        Mustache mustache = mf.compile(templateName);
+        
+        randomizeParameters();
+        
+        HashMap<String, Object> scopes = setUpScope();
+        
+        StringWriter writer = new StringWriter();
+        mustache.execute(writer, scopes);
+        
+        Question question = setUpQuestionAndAnswers();
+        question.setCode(writer.getBuffer().toString());
+        question.setCorrectAnswer(determineRightAnswer());
+        
+        return question;
+    }
+    
+    abstract void randomizeParameters();
+    abstract HashMap<String, Object> setUpScope();
+    abstract Question setUpQuestionAndAnswers();
+    abstract String determineRightAnswer();
+
+    public String getTemplateName() {
+        return templateName;
+    }
+
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
 }

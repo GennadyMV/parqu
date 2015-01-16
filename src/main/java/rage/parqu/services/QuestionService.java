18640f0427ded3fd99db2fc3824dfa1b9665eb06
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-01-16 11:14:47 +0200

    saving time of answer

--- src/main/java/rage/parqu/services/QuestionService.java
+++ src/main/java/rage/parqu/services/QuestionService.java
@@ -58,7 +58,7 @@ public class QuestionService {
         }
 
         boolean correct = question.getCorrectAnswer().equals(check.getAnswer());
-        answerRepository.save(new DbAnswer(check.getStudentID(), correct, question.getParameters(), check.getQuestionID(), check.getAnswer(), question.getTimeStamp()));
+        answerRepository.save(new DbAnswer(check.getStudentID(), correct, question.getParameters(), check.getQuestionID(), check.getAnswer(), new DateTime()));
         if (correct) {
             questionsOnHold.remove(check.getAnswerID());
         }

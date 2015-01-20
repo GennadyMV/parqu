d68fe71f19157c650a7db4ac18a065300472b92f
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-01-20 14:06:05 +0200

    smaller OR equals

--- src/main/java/rage/parqu/questioncreator/CharAtQuestionCreator.java
+++ src/main/java/rage/parqu/questioncreator/CharAtQuestionCreator.java
@@ -60,7 +60,7 @@ public class CharAtQuestionCreator extends QuestionCreator{
         int jStart = determineStartingIndex(second, secondIndex);
         
         for (int i = 0; i <= 2; i++) {
-            for (int j = 0; j < 2; j++) {
+            for (int j = 0; j <= 2; j++) {
                 answers.add("" + first.charAt(iStart + i) + second.charAt(jStart + j));        
             }
         }        

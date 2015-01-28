ec4bfb3e14b31b16dc2b77ba7856d86e24ae4a6c
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-01-28 16:32:18 +0200

    adding exercise about overloading functions

---
+++ src/main/java/rage/parqu/questioncreator/OverloadingMethodQuestionCreator.java
@@ -0,0 +1,43 @@
+package rage.parqu.questioncreator;
+
+import java.util.Set;
+import java.util.TreeSet;
+import rage.parqu.abstractquestioncreators.TwoValueQuestionCreator;
+import rage.parqu.domain.Question;
+
+public class OverloadingMethodQuestionCreator extends TwoValueQuestionCreator{
+
+    public OverloadingMethodQuestionCreator() {
+        super.setTemplateName("overloadingmethods.mustache");
+    }
+
+    @Override
+    protected Question setUpQuestionAndAnswers() {
+        Question question = new Question();
+        question.setQuestionText("Mikä arvo tulostetaan?");
+        question.setAnswers(buildAnswers());
+        return question;       
+    }
+    
+    private String[] buildAnswers() {
+        Set<String> answers = new TreeSet();
+        
+        answerPlusTwo(answers, first);
+        answerPlusTwo(answers, second);
+        answerPlusTwo(answers, (first + second));
+        
+        return answers.toArray(new String[0]);
+    }
+
+    private void answerPlusTwo(Set<String> answers, int value) {
+        for (int i = 0; i <= 2; i++) {
+            answers.add("" + (value + i));
+        }
+    }
+
+    @Override
+    protected String determineRightAnswer() {
+        return "" + (first + second + 2);
+    }
+    
+}

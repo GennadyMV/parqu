e99a6ea79f82e3fabefcacf5ebf309d0c6311b23
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-01-29 16:49:28 +0200

    made options better for overloaded methods

--- src/main/java/rage/parqu/questioncreator/OverloadingMethodQuestionCreator.java
+++ src/main/java/rage/parqu/questioncreator/OverloadingMethodQuestionCreator.java
@@ -5,7 +5,7 @@ import java.util.TreeSet;
 import rage.parqu.abstractquestioncreators.TwoValueQuestionCreator;
 import rage.parqu.domain.Question;
 
-public class OverloadingMethodQuestionCreator extends TwoValueQuestionCreator{
+public class OverloadingMethodQuestionCreator extends TwoValueQuestionCreator {
 
     public OverloadingMethodQuestionCreator() {
         super.setTemplateName("overloadingmethods.mustache");
@@ -16,23 +16,23 @@ public class OverloadingMethodQuestionCreator extends TwoValueQuestionCreator{
         Question question = new Question();
         question.setQuestionText("Mikä arvo tulostetaan?");
         question.setAnswers(buildAnswers());
-        return question;       
+        return question;
     }
-    
+
     private String[] buildAnswers() {
         Set<String> answers = new TreeSet();
-        
+
         answerPlusTwo(answers, first);
         answerPlusTwo(answers, second);
         answerPlusTwo(answers, (first + second));
-        
+
         return answers.toArray(new String[0]);
     }
 
     private void answerPlusTwo(Set<String> answers, int value) {
         for (int i = 0; i <= 2; i++) {
             answers.add("" + (value + i));
-            answers.add("" + (value * i));
+            answers.add("" + (value * (i + 1)));
         }
     }
 
@@ -40,5 +40,5 @@ public class OverloadingMethodQuestionCreator extends TwoValueQuestionCreator{
     protected String determineRightAnswer() {
         return "" + (first + second + 2);
     }
-    
+
 }

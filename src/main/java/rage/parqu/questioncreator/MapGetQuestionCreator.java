306a7a778992c11f684f873f153fae02b5d89428
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-03-03 14:52:30 +0200

    new exercise about maps

---
+++ src/main/java/rage/parqu/questioncreator/MapGetQuestionCreator.java
@@ -0,0 +1,72 @@
+package rage.parqu.questioncreator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import rage.parqu.abstractquestioncreators.QuestionCreator;
+import rage.parqu.domain.Question;
+import static rage.parqu.util.Randomizer.randomPositiveIntegerFromZero;
+
+public class MapGetQuestionCreator extends QuestionCreator {
+
+    private List<Integer> numberStrings;
+    private List<Integer> numbers;
+    private int index;
+
+    public MapGetQuestionCreator() {
+        super.setTemplateName("mapget.mustache");
+    }
+
+    @Override
+    protected void randomizeParameters() {
+        numbers = new ArrayList();
+        while (numbers.size() < 3) {
+            int newNumber = randomPositiveIntegerFromZero(10);
+            if (!numbers.contains(newNumber)) {
+                numbers.add(newNumber);
+            }
+        }
+        for (int i = numbers.size() - 1; i >= 0 ; i--) {
+            numberStrings.add(numbers.get(i));
+        }
+        index = randomPositiveIntegerFromZero(2);
+    }
+
+    @Override
+    protected HashMap<String, Object> setUpScope() {
+        HashMap<String, Object> scopes = new HashMap<>();
+        for (int i = 0; i < numbers.size(); i++) {
+            scopes.put("number" + (i+1), numbers.get(i));
+            scopes.put("numberString" + (i + 1), numberStrings.get(i));
+        }
+        return scopes;
+    }
+
+    @Override
+    protected Question setUpQuestionAndAnswers() {
+        Question question = new Question();
+        question.setQuestionText("Mikä numero poistetaan listalta?");
+        question.setAnswers(setupAnswers());
+        return question;
+    }
+
+    @Override
+    protected String determineRightAnswer() {
+        return "muutostaulu.get(" + numberStrings.get(index) + ")";
+    }
+
+    private Set<String> setupAnswers() {
+        Set<String> answers = new HashSet<>();
+        
+        for (int i = 0; i < numbers.size(); i++) {
+            answers.add("muutostaulu.get(" + numbers.get(i) + ")");
+            answers.add("muutostaulu.get(\"" + numbers.get(i) + "\")");
+        }
+        
+        return answers;
+    }
+
+
+}
